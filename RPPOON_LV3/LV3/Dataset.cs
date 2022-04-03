1c24aea53d4de7b058da8aed7f6ddf4f81d32378
tialedencan <tialedencan@gmail.com>
2022-04-03 17:52:03 +0200

    Prototype and Dataset

---
+++ RPPOON_LV3/LV3/Dataset.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV3
+{
+    class Dataset:Prototype
+    {
+        private List<List<string>> data; //list of lists of strings
+        public Dataset()
+        {
+            this.data = new List<List<string>>();
+        }
+        public Dataset(string filePath) : this()
+        {
+            this.LoadDataFromCSV(filePath);
+        }
+        public void LoadDataFromCSV(string filePath)
+        {
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    List<string> row = new List<string>();
+                    string[] items = line.Split(',');
+                    foreach (string item in items)
+                    {
+                        row.Add(item);
+                    }
+                    this.data.Add(row);
+                }
+            }
+        }
+        public IList<List<string>> GetData()
+        {
+            return
+            new System.Collections.ObjectModel.ReadOnlyCollection<List<string>>(data);
+        }
+        public void ClearData()
+        {
+            this.data.Clear();
+        }
+
+        public Prototype Clone()
+        {
+            return (Prototype)this.MemberwiseClone();
+        }
+    }
+}
---
+++ RPPOON_LV3/LV3/Prototype.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV3
+{
+    interface Prototype
+    {   
+        Prototype Clone();
+    }
+}
