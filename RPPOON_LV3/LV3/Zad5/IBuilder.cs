3f8f27863c5e35e463090de92bceeb9902c4c6f6
tialedencan <tialedencan@gmail.com>
2022-04-08 00:32:34 +0200

    IBuilder, NotificationBuilder and NotificationDirector

--- RPPOON_LV3/LV3/Zad5/IBuilder.cs
+++ RPPOON_LV3/LV3/Zad5/IBuilder.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using LV3.Zad4;
 
 namespace LV3.Zad5
 {
-    internal interface IBuilder
+     interface IBuilder
     {
+        ConsoleNotification Build();
+        IBuilder SetAuthor(String author);
+        IBuilder SetTitle(String title);
+        IBuilder SetTime(DateTime time);
+        IBuilder SetLevel(Category level);
+        IBuilder SetColor(ConsoleColor color);
+        IBuilder SetText(String text);
     }
 }
--- RPPOON_LV3/LV3/Zad5/NotificationBuilder.cs
+++ RPPOON_LV3/LV3/Zad5/NotificationBuilder.cs
@@ -1,10 +1,55 @@
-﻿using System;
+﻿using LV3.Zad4;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace LV3.Zad5
 {
-    internal class NotificationBuilder
-    {
+    class NotificationBuilder : IBuilder
+    {   //trebam defaultne vrijednosti atributa
+        private string author;
+        private string title;
+        private string text;
+        private DateTime time;
+        private Category level;
+        private ConsoleColor color;
+        public ConsoleNotification Build()
+        {
+            return new ConsoleNotification(author, title, text, time, level, color);
+        }
+
+        public IBuilder SetAuthor(string author)
+        {
+            this.author = author; 
+            return this;
+        }
+ 
+        public IBuilder SetTitle(string title)
+        {
+           this.title = title;
+            return this;
+        }
+        public IBuilder SetTime(DateTime time)
+        {
+           this.time= time;
+            return this;
+        }
+
+        public IBuilder SetLevel(Category level)
+        {
+           this.level = level;
+            return this;
+        }
+        public IBuilder SetColor(ConsoleColor color)
+        {
+            this.color = color;
+            return this;
+        }
+        public IBuilder SetText(string text)
+        {
+            this.text = text;
+            return this;
+        }
     }
+      
 }
