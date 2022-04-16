a02c854991188daa27d2bf63382acf0acd707190
tialedencan <tialedencan@gmail.com>
2022-04-17 00:06:18 +0200

    Discounted items

---
+++ LV4/Decorator/DiscountedItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator
+{ //Klasa ima atribut za postotak popusta, koji se rabi u metodama propisanim sučeljem tako da se cijena umanjuje za dani postotak,
+  //a u opis se uključuje „now at [XY]% off!“ nakon ispisivanja osnovnog opisa.
+    class DiscountedItem : RentableDecorator
+    {
+        private int discountPercentage;
+        public DiscountedItem(IRentable rentable, int discountPercentage) : base(rentable)
+        {
+            this.discountPercentage = discountPercentage;
+        }
+        public override double CalculatePrice()
+        {
+            return base.CalculatePrice()-(base.CalculatePrice()*(discountPercentage/100));
+        }
+        public override string Description
+        {
+            get
+            {
+                return  base.Description + $" now at {discountPercentage}% off!";
+            }
+        }
+    }
+}
--- LV4/Decorator/Program.cs
+++ LV4/Decorator/Program.cs
@@ -14,6 +14,13 @@ namespace Decorator
             printer.DisplayItems(rentableItems);
             Console.WriteLine("Total price:");
             printer.PrintTotalPrice(rentableItems);
+
+            List<IRentable> flashSale = new List<IRentable>() { new DiscountedItem(rentableItems[0],20), new DiscountedItem(rentableItems[1], 10),
+                                                    new DiscountedItem(rentableItems[2],10),new DiscountedItem(rentableItems[3],15)};
+            Console.WriteLine("Items:");
+            printer.DisplayItems(flashSale);
+            printer.PrintTotalPrice(flashSale); 
+
         }
     }
 }
