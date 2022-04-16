45f2ad5cbf3f306f76a9ab0c5941ef79e15cceb6
tialedencan <tialedencan@gmail.com>
2022-04-16 23:02:28 +0200

    Adapter

---
+++ LV4/LV4/Adapter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV4
+{
+    class Adapter
+    {
+        private Analyzer3rdParty analyticsService;
+        public Adapter(Analyzer3rdParty service)
+        {
+            this.analyticsService = service;
+        }
+        private double[][] ConvertData(Dataset dataset)
+        {
+            IList<List<double>> data = dataset.GetData();
+            double[][] convertedData= new double[data.Count][];
+            for (int i = 0; i < data.Count; i++)
+            {
+                convertedData[i] = new double[data[i].Count];
+                for(int j = 0; j < data[i].Count; j++)
+                {
+                    convertedData[i][j] = data[i][j];
+                }
+            }
+            return convertedData;
+        }
+        public double[] CalculateAveragePerColumn(Dataset dataset)
+        {
+            double[][] data = this.ConvertData(dataset);
+            return this.analyticsService.PerColumnAverage(data);
+        }
+        public double[] CalculateAveragePerRow(Dataset dataset)
+        {
+            double[][] data = this.ConvertData(dataset);
+            return this.analyticsService.PerRowAverage(data);
+        }
+    }
+}
---
+++ LV4/LV4/Analyzer3rdParty.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LV4
+{
+    class Analyzer3rdParty
+    {
+        public double[] PerRowAverage(double[][] data)
+        {
+            int rowCount = data.Length;
+            double[] results = new double[rowCount];
+            for (int i = 0; i < rowCount; i++)
+            {
+                results[i] = data[i].Average();
+            }
+            return results;
+        }
+        public double[] PerColumnAverage(double[][] data)
+        {
+            int rowCount = data.Length;
+            int columnCount = data[0].Length;
+            double[] results = new double[columnCount];
+            for (int i = 0; i < columnCount; i++)
+            {
+                for(int j = 0; j < rowCount; j++)
+                {
+                    results[i] += data[j][i];
+                }
+                results[i] /= rowCount;
+            }
+            return results;
+        }
+    }
+}
