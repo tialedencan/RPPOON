20db27bbe83feb5968648e32a6139bb11f25e922
tialedencan <tialedencan@gmail.com>
2022-04-17 22:13:26 +0200

    EmailValidator, testing EmailValidator and PasswordValidator

---
+++ LV4/Facade/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Facade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string email = "world@gmail.com";
+            string password = "Wor1D";
+            EmailValidator emailValidator = new EmailValidator();
+            PasswordValidator passwordValidator = new PasswordValidator(5);
+            if (emailValidator.IsValidAddress(email))
+            {
+                Console.WriteLine("Email address is valid.");
+            }
+            else { Console.WriteLine("Email address is not valid."); }
+            if (passwordValidator.IsValidPassword(password))
+            {
+                Console.WriteLine("Password address is valid.");
+            }
+            else { Console.WriteLine("Password is not valid."); }
+        }
+    }
+}
