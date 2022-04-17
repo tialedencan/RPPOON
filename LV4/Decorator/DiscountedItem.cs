d5b7d5ddf359ec8b6bc03f62c2603cff846a1eb2
tialedencan <tialedencan@gmail.com>
2022-04-17 21:15:04 +0200

    Testing discounted items

--- LV4/Decorator/DiscountedItem.cs
+++ LV4/Decorator/DiscountedItem.cs
@@ -13,8 +13,19 @@ namespace Decorator
             this.discountPercentage = discountPercentage;
         }
         public override double CalculatePrice()
-        {
-            return base.CalculatePrice()-(base.CalculatePrice()*(discountPercentage/100));
+        {   
+            double price=base.CalculatePrice();
+            double percentage = 0;
+            if(discountPercentage/10 < 10 && discountPercentage/10 >= 0)
+            {
+                for(int i=0; i< discountPercentage / 10; i++)
+                {
+                    percentage += 0.1;
+                }
+            }
+            else { percentage = 1; }
+            
+            return price*(1-percentage);
         }
         public override string Description
         {
