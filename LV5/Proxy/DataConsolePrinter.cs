31ed35cb6c2ef2440e08e8c7009e9a9737be9851
tialedencan <tialedencan@gmail.com>
2022-05-01 21:13:05 +0200

    Testing Proxy

--- LV5/Proxy/DataConsolePrinter.cs
+++ LV5/Proxy/DataConsolePrinter.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace Proxy
 {
     class DataConsolePrinter
     {
-
+        public void PrintData(IDataset dataset)
+        {
+            ReadOnlyCollection<List<string>> data = dataset.GetData();
+            foreach(var item in data)
+            {
+                foreach(string value in item)
+                {
+                    Console.Write(value + " ");
+                }
+               Console.WriteLine();
+            }
+                
+        }
+        
     }
 }
--- LV5/Proxy/Program.cs
+++ LV5/Proxy/Program.cs
@@ -6,7 +6,16 @@ namespace Proxy
     {
         static void Main(string[] args)
         {
-           
+            User user = User.GenerateUser("John");
+            ProtectionProxyDataset protectionProxy = new ProtectionProxyDataset(user);
+            VirtualProxyDataset virtualProxy = new VirtualProxyDataset("sensitiveData.csv");
+
+            DataConsolePrinter printer = new DataConsolePrinter();
+            Console.WriteLine("Protection Proxy:");
+            printer.PrintData(protectionProxy);
+            Console.WriteLine("Virtual Proxy:");
+            printer.PrintData(virtualProxy);
+
         }
     }
 }
