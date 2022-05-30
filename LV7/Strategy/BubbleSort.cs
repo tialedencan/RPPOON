f83b2e566ab4ed16dfe951f60c069e5eb64a952e
tialedencan <tialedencan@gmail.com>
2022-05-31 00:10:39 +0200

    Sort Strategy

---
+++ LV7/Strategy/BubbleSort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy
+{
+    class BubbleSort:SortStrategy
+    {
+        public override void Sort(double[] array)
+        {
+            int arraySize=array.Length;
+            for (int i=0; i<arraySize; i++)
+            {
+                for(int j=0; j<arraySize-i-1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j],ref array[j + 1]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LV7/Strategy/SequentialSort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy
+{
+    class SequentialSort:SortStrategy
+    {
+        public override void Sort(double[] array)
+        {
+            int arraySize = array.Length;
+            for (int i = 0; i < arraySize - 1; i++)
+            {
+                for (int j = i + 1; j < arraySize; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        Swap(ref array[i], ref array[j]);
+                    }
+                }
+            }
+        }
+    }
+}
